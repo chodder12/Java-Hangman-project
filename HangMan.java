89d208927f0bc183fd36450ad7a83a0b3a3da366
chodder12 <105743174+chodder12@users.noreply.github.com>
2022-08-04 20:18:02 -0230

    Add files via upload

--- HangMan.java
+++ HangMan.java
@@ -2,10 +2,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-public class HangMan {
+public abstract class HangMan {
     public static void main(String[] args) throws FileNotFoundException {
         //scanner object to read text file of words
         Scanner scanner = new Scanner(new File("C:/JAVA2/JAVA2HANGMANPROJECT/HangMan/hangmanWords.txt"));
+
+        //scanner for guess input
+        Scanner input = new Scanner(System.in);
 
         //creating array list to add words
         List<String> HangmanWords = new ArrayList<>();
@@ -22,17 +25,36 @@ public class HangMan {
 
         System.out.println(word);
 
-        List<Character> guesses = new ArrayList<>();
+        //creating a list of guesses
+        List<Character> Guesses = new ArrayList<>();
 
-            for (int i = 0; i < word.length(); i++) {
-                    if (guesses.contains(word.charAt(i))) {
-                        System.out.print(word);
-                    } else {
-                        System.out.print("_");
-                    }
-            }
-        }
+        //calling printword method
+        WordBlanks(word, Guesses);
+
+        System.out.println("enter a letter");
+
+        String guess = input.nextLine();
+        //only taking first string of user input
+        Guesses.add(guess.charAt(0));
+
     }
+
+    //method to print a _ for every letter of the word
+        private static void WordBlanks(String word, List<Character> Guesses){
+            for (int i = 0; i < word.length(); i++) {
+                if (Guesses.contains(word.charAt(i))) {
+                    System.out.print(word.charAt(i));
+                } else {
+                    System.out.print("_");
+                }
+            }
+            System.out.println();
+        }
+
+
+
+
+}
 
 
 
